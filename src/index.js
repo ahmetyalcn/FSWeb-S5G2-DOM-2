3ee6cde30d7a60db5a668d4ae80966a5a8367974
Ahmet Necdet Yalçın <ahmetnecdetyalcin@Ahmet-MacBook-Air.local>
2023-05-26 17:54:15 +0300

    güncellendi

--- src/index.js
+++ src/index.js
@@ -7,3 +7,41 @@ import './less/index.less'
 
 
 // Kodlar buraya gelecek!
+
+document.querySelectorAll("img").forEach(resim => {
+  resim.addEventListener("mouseover", (e) => {
+    e.target.style.filter = "grayscale(100%)";
+  });
+  resim.addEventListener("mouseout", (e) => {
+    e.target.style.filter = "grayscale(0)";
+  });
+});
+
+
+window.addEventListener("load", ()=>{
+    setTimeout(()=>{
+        document.querySelector("body").style.background="gray"
+    },2000)
+    
+})
+
+let handler = (e)=>{
+    e.target.style.width = "50%"
+}
+let dblhandler = (e)=>{
+    e.target.style.width = "100%"
+}
+document.querySelectorAll("img").forEach((resim)=>{
+    resim.addEventListener("click", handler)
+    resim.addEventListener("dblclick", dblhandler)
+})
+
+document.addEventListener("keydown",(event)=>{
+    document.querySelectorAll("img").forEach((resim)=>{
+        if (event.key === "a") {
+            resim.removeEventListener("click", handler)
+          }
+
+    })
+})
+    
